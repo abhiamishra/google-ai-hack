8a882fffcee5f5330e3098aaf071069ae3b30a8e
mike marambio <mikemarambio@gmail.com>
2024-04-17 13:56:10 -0500

    adding base connection to firebase

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import firestore
 
 app = FastAPI()
 
+creds = credentials.Certificate("./borderless-asmr-firebase-adminsdk.json")
 
+# Initialize the app with a None auth variable, limiting the server's access
+firebase_admin.initialize_app(creds)
+db = firestore.client()
+
+# The app only has access to public data as defined in the Security Rules
+# ref = db.reference('/test')
+collection = db.collection('test').document('MYfL7vSJPmzYCPO6U9HX')
+data = collection.get().to_dict()
+print(data)
+# print(ref.get())
+# print(default_app)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
