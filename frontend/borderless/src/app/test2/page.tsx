ad0ef62aab891e6942bd3b58a6b6aa258685e505
mike marambio <mikemarambio@gmail.com>
2024-04-22 23:27:45 -0500

    removed null side bar

--- frontend/borderless/src/app/test2/page.tsx
+++ frontend/borderless/src/app/test2/page.tsx
@@ -169,7 +169,6 @@ export default function useBecomeVendor() {
                   <label htmlFor="fullName" className="sr-only">
                     Start Date
                   </label>
-                  <span>{JSON.stringify(controlledDate)}</span>
                 <Controller
                   control={control}
                   name="startDate"
